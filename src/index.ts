07cf47b7325e1da27c83f2df25165879b1575ae5
Rafael Uchoa <rafaeluchoa.1996@gmail.com>
2020-06-22 23:13:36 -0300

    Login setup

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,55 @@ import 'reflect-metadata';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-
 import { createConnection } from 'typeorm';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import cors from 'cors';
 
+import MeResolver from './modules/user/Me';
 import RegisterResolver from './modules/user/Register';
+import LoginResolver from './modules/user/Login';
+import { redis } from './redis';
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [MeResolver, RegisterResolver, LoginResolver],
   });
 
   const apolloServer = new ApolloServer({
     schema,
+    context: ({ req }: any) => ({ req }),
   });
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: 'http://localhost:3000',
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any,
+      }),
+      name: 'qid',
+      secret: 'esxvlyhmmo',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
+      },
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
